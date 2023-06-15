20c264427ac58f33c526f13706b615f5e0b523a2
John mk UBUNTU <jmakateto@gmail.com>
2023-06-15 22:57:11 +0300

    completed assignement

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+// Challenge 1
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+// Challenge 2
+let random = Math.floor(Math.random() * 10) + 1;
+
+// Challenge 3
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+// Challenge 4
+let max = Math.max(5, 10, 15, 20);
+
+// Testing the results
+console.log("multiply:", multiply);
+console.log("random:", random);
+console.log("mod:", mod);
+console.log("max:", max);
